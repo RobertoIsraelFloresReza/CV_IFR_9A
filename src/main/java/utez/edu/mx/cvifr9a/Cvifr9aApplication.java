63a9e423fdba7c82aaa0d0929922dea821c443a5
RobertoIsraelFloresReza <20223tn016@utez.edu.mx>
2025-05-26 17:48:24 -0600

    [FIRST COMMIT]

---
+++ src/main/java/utez/edu/mx/cvifr9a/Cvifr9aApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvifr9a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvifr9aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvifr9aApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/cvifr9a/Cvifr9aApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvifr9a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvifr9aApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
